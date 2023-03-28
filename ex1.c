bacee153f537835465a364211ca7f3477f8fa3e9
hong0678 <79080434+hong0678@users.noreply.github.com>
2023-03-28 17:00:26 +0900

    2023.03.28 - hong

---
+++ ex1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*int main(void) {
+	printf("마트에서 사올 품목\n");
+	printf("=====================\n");
+	printf("사과, 우유, 빵\n");
+	printf("=====================\n");
+
+	return 0;
+
+}
+int main(void) {
+	printf("안녕하세요\n");
+	printf("이번 코드에는 많은 오류가 있다네요\n");
+	printf("제가 다 고쳐보겠습니다\n");
+
+	return 0;
+}*/
+//덧셈 프로그램#1
+/*int main(void) {
+	int x;
+	int y;
+	int sum;
+	x = 100;
+	y = 200;
+	sum = x + y;
+	printf("두 수의 합 = %d", sum);
+
+	return 0;
+}*/
+/*int main(void) {
+	int x = 100;
+	int y = 300;
+	int sum = x + y;
+
+	printf("두 수의 합 = %d", sum);
+
+	return 0;
+}*/
